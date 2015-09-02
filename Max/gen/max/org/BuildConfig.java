9246a43d77c5315a35c87e1d9a324c93ffa2ee4d
www.david777@mail.ru <www.david777@mail.ru>
2015-09-02 16:38:26 +0400

    Android Application

---
+++ Max/gen/max/org/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package max.org;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ Max/src/max/org/MaxActivity.java
@@ -0,0 +1,58 @@
+package max.org;
+
+import android.app.Activity;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DigitalClock;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+
+public class MaxActivity extends Activity {
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+		LinearLayout a = (LinearLayout) findViewById(R.id.List);
+		Button David = new Button(a.getContext());
+		David.setTextColor(Color.RED);
+		David.setText("David");
+		a.addView(David);
+		Button Mher = (Button) findViewById(R.id.Mher);
+		Button clear = (Button) findViewById(R.id.clear);
+		final EditText David_T = new EditText(a.getContext());
+		David_T.setHeight(100);
+		a.addView(David_T);
+		David.setOnClickListener(new View.OnClickListener() {
+
+			public void onClick(View v) {
+				David_T.setText(David_T.getText() + "David");
+				// TODO Auto-generated method stub
+			}
+		});
+
+		final EditText Mher_T = (EditText) findViewById(R.id.Mher_TEXT);
+		Mher.setOnClickListener(new View.OnClickListener() {
+
+			public void onClick(View v) {
+				Mher_T.setText(Mher_T.getText() + "Mher");
+
+				// TODO Auto-generated method stub
+			}
+		});
+
+		clear.setOnClickListener(new View.OnClickListener() {
+			
+			public void onClick(View v) {
+				Mher_T.setText("");
+			}
+		});
+		DigitalClock Clock_D = (DigitalClock) findViewById(R.id.Clock_D);
+		Clock_D.setBackgroundColor(Color.GREEN);
+		Clock_D.setSoundEffectsEnabled(true);
+		Clock_D.setTextColor(Color.BLUE);
+		Clock_D.setTextSize(60);
+	}
+}
