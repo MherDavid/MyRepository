83b6dd6bc6836a4eaf1f59c93d75b83439844be3
www.david777@mail.ru <www.david777@mail.ru>
2015-09-17 18:10:09 +0400

    Create a new android project

---
+++ PhoneApp/src/com/example/phoneapp/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.phoneapp;
+
+//import android.support.v7.app.ActionBarActivity;
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.app.Activity;
+
+@SuppressLint("NewApi")
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+	}
+
+	
+}
