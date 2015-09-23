0f7d569ce4473af34070ed8faa5cb727341582f6
www.david777@mail.ru <www.david777@mail.ru>
2015-09-23 18:16:08 +0400

    Edit the project Added ListView

--- PhoneApp/src/com/example/phoneapp/MainActivity.java
+++ PhoneApp/src/com/example/phoneapp/MainActivity.java
@@ -1,19 +1,35 @@
 package com.example.phoneapp;
 
-//import android.support.v7.app.ActionBarActivity;
-import android.annotation.SuppressLint;
+import java.util.ArrayList;
+import java.util.List;
+
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.app.Activity;
 
-@SuppressLint("NewApi")
+
 public class MainActivity extends Activity {
+
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		ListView gugonerView = (ListView) findViewById(R.id.myListView);
+
 		
+		List<String> gugonerList = new ArrayList<>();
+		
+		for (int i = 0; i < 1000; ++i) {
+			String gugo = "Gugo #" + Integer.toString(i);
+			gugonerList.add(gugo);
+		}
+		
+		ArrayAdapter<String> gugoAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, gugonerList);
+		
+		gugonerView.setAdapter(gugoAdapter);
+
 	}
 
-	
 }
