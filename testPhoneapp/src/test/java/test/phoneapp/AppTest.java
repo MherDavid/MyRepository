c1741a3ce5f6478000351a3bc98ecf54a1ab400e
www.david777@mail.ru <www.david777@mail.ru>
2015-09-24 18:07:25 +0400

    some changes

--- testPhoneapp/src/test/java/test/phoneapp/AppTest.java
+++ testPhoneapp/src/test/java/test/phoneapp/AppTest.java
@@ -16,51 +16,50 @@ import org.openqa.selenium.remote.DesiredCapabilities;
 import org.junit.Before;
 import org.junit.Test;
 
-public	class AppTest{
-	
+public class AppTest {
+
 	private AppiumDriver<AndroidElement> driver;
-	
+
 	@Before
-	public void connectApp()throws MalformedURLException {
+	public void connectApp() throws MalformedURLException {
 		System.out.println("-----Run the application------");
 		// set up appium
 		DesiredCapabilities capabilities = new DesiredCapabilities();
-		capabilities.setCapability("deviceName","TestPhone");
+		capabilities.setCapability("deviceName", "TestPhone");
 		capabilities.setCapability("platformVersion", "4.4.2");
-		capabilities.setCapability("app", "/home/student/workspace/MyRepository/PhoneApp/bin/PhoneApp.apk");
+		capabilities
+				.setCapability("app",
+						"/home/student/workspace/MyRepository/PhoneApp/bin/PhoneApp.apk");
 		capabilities.setCapability("appPackage", "com.example.phoneapp");
 		capabilities.setCapability("appActivity", ".MainActivity");
-		driver = new AndroidDriver<AndroidElement>(new URL("http://0.0.0.0:4723/wd/hub"), capabilities);
+		driver = new AndroidDriver<AndroidElement>(new URL(
+				"http://0.0.0.0:4723/wd/hub"), capabilities);
 	}
+
 	/*
-	public void text(@Named("ourText") String ourText, @Named("id") String id){
-		AndroidElement click = driver.findElement(By.id(id));
-		click.sendKeys(ourText);
-	}
-	public void checkTextEditor(@Named("expectedText") String expectedText, @Named("id") String id){
-	try{
-		AndroidElement click = driver.findElement(By.id(id));
-		String realText = click.getText();
-		Assert.assertEquals(expectedText, realText);
-	} catch(Exception e) {
-		e.printStackTrace();	
-	}
-}*/
-		@Test
-		public void scroll() throws InterruptedException{
-		/*driver.scrollTo("Click").click();
-		driver.findElement(By.id("imageSwitcher1")).click();*/
+	 * public void text(@Named("ourText") String ourText, @Named("id") String
+	 * id){ AndroidElement click = driver.findElement(By.id(id));
+	 * click.sendKeys(ourText); } public void
+	 * checkTextEditor(@Named("expectedText") String expectedText, @Named("id")
+	 * String id){ try{ AndroidElement click = driver.findElement(By.id(id));
+	 * String realText = click.getText(); Assert.assertEquals(expectedText,
+	 * realText); } catch(Exception e) { e.printStackTrace(); } }
+	 */
+	@Test
+	public void scroll() throws InterruptedException {
+		/*
+		 * driver.scrollTo("Click").click();
+		 * driver.findElement(By.id("imageSwitcher1")).click();
+		 */
 		AndroidElement listView = driver.findElement(By.id("myListView"));
 		Point location = listView.getCenter();
-	    Dimension size = listView.getSize();
+		Dimension size = listView.getSize();
 
 		Thread.sleep(5000);
 		int startY = (int) (location.y + size.height * 0.4);
-	    int endY = (int) (location.y - size.height * 0.4); 
-	       
-	      driver.swipe(location.x, startY, location.x, endY, 5000);
-		
-		
-		
+		int endY = (int) (location.y - size.height * 0.4);
+
+		driver.swipe(location.x, startY, location.x, endY, 5000);
+
 	}
 }
